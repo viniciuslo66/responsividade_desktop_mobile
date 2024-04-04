ca9629a4e820bcff762b0780af59f452673eec2f
viniciuslo66 <viniciuslo66@gmail.com>
2024-04-04 10:37:54 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/viniciuslo66/responsividade_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.viniciuslo66.responsividade_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:responsividade_flutter/responsive/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileBody;
+  final Widget desktopBody;
+
+  const ResponsiveLayout({
+    super.key,
+    required this.mobileBody,
+    required this.desktopBody,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < mobileWidth) {
+          return mobileBody;
+        } else {
+          return desktopBody;
+        }
+      },
+    );
+  }
+}
